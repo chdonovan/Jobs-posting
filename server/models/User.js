7552b8ea5ce34ac6fcdabfe779add7e005de9dd2
chdonovan <chrisdonovan311@gmail.com>
2021-08-18 20:03:00 -0500

    jobs started

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ const mongoose  = require('mongoose');
 
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
-//const Jobs = require('./Jobs');
+const Jobs = require('./Jobs');
 
 const userSchema = new Schema ({
     firstName: {
