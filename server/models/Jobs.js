3398d74e01090e68396c84c4b678ce514cf9df28
chdonovan <chrisdonovan311@gmail.com>
2021-08-18 20:44:03 -0500

    jobs

---
+++ server/models/Jobs.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const {Schema } = mongoose;
+
+const jobsSchema = new Schema ({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: String,
+        require: true,
+    },
+     category: {
+        type: Schema.Types.ObjectId,
+        red: 'Category',
+        required: true
+    },
+    location: {
+        address: String
+    }
+});
+
+
