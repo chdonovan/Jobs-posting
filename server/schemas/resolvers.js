80421809a8f75662d3a14bfa91e96ad648a4b3e1
reinhardtc <reinhardtc22@gmail.com>
2021-08-23 15:51:21 -0500

    all resolvers added. needs review

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
-const { User, Job } = require('../models');
+const { User, Job, Response } = require('../models');
 
 const resolvers = {
   Query: {
@@ -13,21 +13,34 @@ const resolvers = {
         params.category = category;
       }
     },
-    jobs: async (parent, { _id }) => {
+    job: async (parent, { _id }) => {
       return await Job.findById(_id).populate('category');
     },
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'jobs.responses', 
+          path: 'jobs.responses',
           populate: 'category',
         });
 
         // do we want to sort by created date of the job posting?
-        // user.job.sort((a, b) => b.createdDate - a.createdDate)
+        user.job.sort((a, b) => b.createdDate - a.createdDate);
 
         return user;
       }
+    },
+    users: async () => {
+      return User.find().select('-__v -password').populate('jobs');
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('jobs');
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
     },
   },
   Mutation: {
@@ -37,24 +50,46 @@ const resolvers = {
 
       return { token, user };
     },
-    addJob: async (parent, { job }, context) => {
+    addThought: async (parent, args, context) => {
       if (context.user) {
-        const newJob = new Job({ jobs});
+        const job = await Job.create({ ...args, username: context.user.username });
 
-        await User.findByIdAndUpdate(context.user._id, { $push: { job: newJob } });
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { jobs: job._id } },
+          { new: true }
+        );
 
-        return newJob;
+        return job;
       }
+
+      throw new AuthenticationError('You need to be logged in!');
     },
     updateUser: async (parent, args, context) => {
       if (context.user) {
         return await user.findByIdAndUpdate(context.user._id, args, { new: true });
       }
-      throw new AuthenticationError('You are not logged in');
+      throw new AuthenticationError('You are not logged in!');
+    },
+    updatedJob: async (parent, args, context) => {
+      if (context.job) {
+        return await job.findByIdAndUpdate(context.job._id, args, { new: true });
+      }
+      throw new AuthenticationError('You must be logged in!');
+    },
+    removeJob: async (parent, args, context) => {
+      if (context.user) {
+        const job = await Job.remove({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { jobs: job._id } },
+          { new: true }
+        );
+      }
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-
       if (!user) {
         throw new AuthenticationError('Incorrect login credentials!');
       }
@@ -64,6 +99,19 @@ const resolvers = {
       }
       const token = signToken(user);
       return { token, user };
+    },
+    addResponse: async (parent, { jobId, responseBody }, context) => {
+      if (context.user) {
+        const updatedJob = await Job.findOneAndUpdate(
+          { _id: jobId },
+          { $push: { responses: { responseBody, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedJob;
+      }
+
+      throw new AuthenticationError('You must be logged in!');
     },
   },
 };
