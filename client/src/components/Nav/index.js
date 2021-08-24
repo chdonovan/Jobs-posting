83d3888865fe165b3d9e8e3124ab87bd5272e396
Kateryna B <butsneva.ekaterina@gmail.com>
2021-08-23 21:30:25 -0500

    setup Dash component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,6 +9,7 @@ import Home from '../../pages/Home'
 import Login from '../../pages/Login';
 import Signup from '../../pages/Signup';
 import Main from '../../pages/Main'
+import Dashboard from "../../pages/Dashboard";
 
 
 const Nav = () => {
@@ -32,99 +33,152 @@ const Nav = () => {
 
 
     return (
-        <HashRouter>
-            <Disclosure as="nav">
-            {({ open }) => (
+      <HashRouter>
+        <Disclosure as="nav">
+          {({ open }) => (
             <>
-                <Disclosure.Panel className="sm:hidden">
-                    <div className="px-2 pt-2 pb-3 space-y-1">
-                        <a key='about' href='#about' className='text-gray-300 hover:bg-gray-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
-                            <NavLink exact to='/' className="nav-link" replace>About</NavLink>
-                        </a>
-                        <a key='contact' className='text-gray-300 hover:bg-gray-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
-                            <NavLink exact to='/' className="nav-link" replace>Contact Us</NavLink>
-                        </a>
-                        <a key='faq' href='#faq' className='text-gray-300 hover:bg-gray-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
-                            <NavLink exact to='/' className="nav-link" replace>FAQ</NavLink>
-                        </a>
-                        <a key='login' className='text-gray-300 hover:bg-gray-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
-                            <NavLink exact to='/Login' className="nav-link" replace>Login</NavLink>
-                        </a>
-                    </div>
-                </Disclosure.Panel>
+              <Disclosure.Panel className="sm:hidden">
+                <div className="px-2 pt-2 pb-3 space-y-1">
+                  <a
+                    key="about"
+                    href="#about"
+                    className="text-gray-300 hover:bg-gray-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    <NavLink exact to="/" className="nav-link" replace>
+                      About
+                    </NavLink>
+                  </a>
+                  <a
+                    key="contact"
+                    className="text-gray-300 hover:bg-gray-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    <NavLink exact to="/" className="nav-link" replace>
+                      Contact Us
+                    </NavLink>
+                  </a>
+                  <a
+                    key="faq"
+                    href="#faq"
+                    className="text-gray-300 hover:bg-gray-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    <NavLink exact to="/" className="nav-link" replace>
+                      FAQ
+                    </NavLink>
+                  </a>
+                  <a
+                    key="login"
+                    className="text-gray-300 hover:bg-gray-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    <NavLink exact to="/Login" className="nav-link" replace>
+                      Login
+                    </NavLink>
+                  </a>
+                </div>
+              </Disclosure.Panel>
 
-
-            <div>
+              <div>
                 {Auth.loggedIn() ? (
-                    <>
-                    <li href='/' onClick={Auth.logout}>Logout</li>
-                    </>
+                  <>
+                    <li href="/" onClick={Auth.logout}>
+                      Logout
+                    </li>
+                  </>
                 ) : (
-                    <div>
-                        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                            <div className="relative flex items-center justify-between h-16">
-                                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                    
-                                    {/* Mobile menu button*/}
-                                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                    <span className="sr-only">Open main menu</span>
-                                    {open ? (
-                                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                                    ) : (
-                                            <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                                        )}
-                                    </Disclosure.Button>
-                                </div>
-                                <div className="sm:block sm:ml-auto sm:mr-auto">
-                                        <img
-                                            className=" h-12"
-                                            src={logo}
-                                            alt="Logo"
-                                        />
-                                </div>
-                                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-end">
-                                    <div className="hidden sm:block sm:ml-6">
-                                        <div className="flex space-x-4">
-                                            <div>
-                                                <a key='about' href='#about'>
-                                                    <NavLink exact to='/' className="nav-link text-gray-600 hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium" replace>About</NavLink>
-                                                </a>
-                                                <a key='about'>
-                                                    <NavLink exact to='/' className="nav-link text-gray-600 hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium" replace>Contact Us</NavLink>
-                                                </a>
-                                                <a key='about' href='#faq'>
-                                                    <NavLink exact to='/Main' className="nav-link text-gray-600 hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium" replace>FAQ MAIN CHANGE</NavLink>
-                                                </a>
-                                                <a key='about'>
-                                                    <NavLink exact to='/Login' className="nav-link text-gray-600 hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium" replace>Login</NavLink>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                  <div>
+                    <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                      <div className="relative flex items-center justify-between h-16">
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                          {/* Mobile menu button*/}
+                          <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                            <span className="sr-only">Open main menu</span>
+                            {open ? (
+                              <XIcon
+                                className="block h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            ) : (
+                              <MenuIcon
+                                className="block h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            )}
+                          </Disclosure.Button>
                         </div>
+                        <div className="sm:block sm:ml-auto sm:mr-auto">
+                          <img className=" h-12" src={logo} alt="Logo" />
+                        </div>
+                        <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-end">
+                          <div className="hidden sm:block sm:ml-6">
+                            <div className="flex space-x-4">
+                              <div>
+                                <a key="about" href="#about">
+                                  <NavLink
+                                    exact
+                                    to="/"
+                                    className="nav-link text-gray-600 hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium"
+                                    replace
+                                  >
+                                    About
+                                  </NavLink>
+                                </a>
+                                <a key="about">
+                                  <NavLink
+                                    exact
+                                    to="/Dashboard"
+                                    className="nav-link text-gray-600 hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium"
+                                    replace
+                                  >
+                                    Contact Us DASH CHANGE
+                                  </NavLink>
+                                </a>
+                                <a key="about" href="#faq">
+                                  <NavLink
+                                    exact
+                                    to="/Main"
+                                    className="nav-link text-gray-600 hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium"
+                                    replace
+                                  >
+                                    FAQ MAIN CHANGE
+                                  </NavLink>
+                                </a>
+                                <a key="about">
+                                  <NavLink
+                                    exact
+                                    to="/Login"
+                                    className="nav-link text-gray-600 hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium"
+                                    replace
+                                  >
+                                    Login
+                                  </NavLink>
+                                </a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
+                  </div>
                 )}
-            </div>
-                        <div className="object-center">
-                            <Switch>
-                                <Route exact path="/" component={Home} />
-                                <Route path="/Login" component={Login} />
-                                <Route path="/Signup" component={Signup} />
-                                <Route path="/Main" component={Main} />
+              </div>
+              <div className="object-center">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/Login" component={Login} />
+                  <Route path="/Signup" component={Signup} />
+                  <Route path="/Main" component={Main} />
+                  <Route path="/Dashboard" component={Dashboard} />
 
-                                {/* <Route path="/Dashboard" component={Dashboard} />
+                  {/* 
                                 <Route path="/Calendar" component={Calendar} />
                                 
                                 <Route path="/Settings" component={Settings} />
                                  */}
-                            </Switch>
-                        </div>
-                        </>
-                        )}
-            </Disclosure>
-        </HashRouter>
+                </Switch>
+              </div>
+            </>
+          )}
+        </Disclosure>
+      </HashRouter>
     );
 };
 
