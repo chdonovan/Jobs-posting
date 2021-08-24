d164c793da73f6afb2ebb4dfffdec9b3ddf39957
reinhardtc <reinhardtc22@gmail.com>
2021-08-24 12:04:27 -0500

    removed all queries and mutations apart from anything needed for signup and login

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,52 +1,22 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Category {
-    _id: ID
-    name: String
-  }
-  type Job {
-    _id: ID
-    title: String
-    description: String
-    price: Float
-    category: Category
-    location: String
-    createdAt: String
-  }
-  type Response {
-    _id: ID
-    responseText: String
-    username: String
-    createdAt: String
-  }
   type User {
     _id: ID
     firstName: String
     lastName: String
     email: String
-    job: [Job]
   }
   type Auth {
     token: ID!
     user: User
   }
   type Query {
-    categories: [Category]
-    jobs(category: ID, name: String): [Job]
-    job(_id: ID!): Job
-    user:(username: String!): User
-    users: [User]
     me: User
   }
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    addJob(job: [ID]!): Job
-    updateUser(firstName: String!, lastName: String! email: String, password: String): User
-    updateJob(_id: ID!, payment: Int!): Job
-    removeJob(_id: ID!): Job
-    login(username: String!, email: String!, password: String!): Auth
-    addResponse(jobId: ID!, responseBody: String!): Job
+    login(email: String!, password: String!): Auth
   }
 `;
 
