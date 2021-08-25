f9a678013130283740fcb72795d9c0cfab6ee2d6
nayala <nathaniel.ayala20@gmail.com>
2021-08-24 23:55:45 -0500

    settings page styled and some setup

--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
-import { Route, NavLink, HashRouter, Switch } from "react-router-dom";
-import { useMutation } from '@apollo/client';
-import { LOGIN } from '../../utils/mutations';
-import Auth from '../../utils/auth';
 
+
+const Settings = props => {
+
+    return(
+            <div>
+                <label>Email</label>
+            </div>
+        );
+};
+
+export default Settings;
