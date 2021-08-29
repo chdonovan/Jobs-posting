2dd8d0ca099c6d92011ea1078cae4c9af3f93f71
Kateryna B <butsneva.ekaterina@gmail.com>
2021-08-29 12:00:42 -0500

    populate user data when loggedin

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -3,6 +3,7 @@ import { LockClosedIcon } from '@heroicons/react/solid';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../../utils/mutations';
 
+
 import Auth from '../../utils/auth';
 
 const SignupForm = () => {
@@ -12,7 +13,7 @@ const SignupForm = () => {
     email: '',
     password: '',
   });
-  const [addUser] = useMutation(ADD_USER);
+  const [addUser, { error }] = useMutation(ADD_USER);
 
     const handleChange = event => {
         const { name, value } = event.target;
@@ -116,7 +117,11 @@ const SignupForm = () => {
                 />
               </div>
             </div>
-
+            {error && (
+              <div className='text-red-700 text-center text-sm'>
+                Sign up failed
+              </div>
+            )}
             <div className='flex justify-center'>
               <button
                 type='submit'
