eb7b8cd5758c4ab1202871902d713433a1adad63
Kateryna B <butsneva.ekaterina@gmail.com>
2021-08-29 15:30:13 -0500

    fixed modal bg opacity

--- client/src/pages/JobPosting.js
+++ client/src/pages/JobPosting.js
@@ -270,7 +270,7 @@ const JobPosting = () => {
                 <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
                   <button
                     type='button'
-                    className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 btn-cust-orange text-base font-medium text-white focus:outline-none sm:ml-3 sm:w-auto sm:text-sm'
+                    className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 btn-cust-orange text-base font-bold text-white focus:outline-none sm:ml-3 sm:w-auto sm:text-sm'
                     onClick={() => setModalOpen(false)}
                     ref={newPostButtonRef}
                   >
@@ -279,7 +279,7 @@ const JobPosting = () => {
                   <NavLink
                     exact
                     to='/'
-                    className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
+                    className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-bold text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
                     replace
                   >
                     Home Page
