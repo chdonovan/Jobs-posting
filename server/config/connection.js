e9a766c2f19578d544290c49495e7b9ffde5c8a5
Colin Reinhardt <reinhardtc22@gmail.com>
2022-01-19 13:59:35 -0600

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'Mongodb://localhost/jobs-posting', {
+mongoose.connect(process.env.MONGODB_URI || 'Mongoldb://localhost/jobs-posting', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
