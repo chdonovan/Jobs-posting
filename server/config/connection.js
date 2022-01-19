36c9b4180d27aa8dbcd582e2e832cb958d6672bd
Colin Reinhardt <reinhardtc22@gmail.com>
2022-01-19 13:52:22 -0600

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dbUser:v2tswTYOf27g9jPV@myjober.bii67.mongodb.net/myjober?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'Mongoldb://localhost/jobs-posting, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
