373abab43fd4b3e9cc54fd01816a50cfd8be6104
reinhardtc <reinhardtc22@gmail.com>
2022-01-19 13:33:45 -0600

    deploying application

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/jobs-posting', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dbUser:v2tswTYOf27g9jPV@myjober.bii67.mongodb.net/myjober?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
